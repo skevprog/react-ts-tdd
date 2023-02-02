87734d8a1c07f93d6d96ee6b9ca093bdf377a332
Kevin Soruco <kevin-s93@hotmail.com>
2023-02-02 16:16:00 -0300

    feat
    * Add Wrapper component

--- src/Wrapper.test.tsx
+++ src/Wrapper.test.tsx
@@ -2,12 +2,18 @@ import { describe, expect, it, } from 'vitest';
 import { render, screen } from '@testing-library/react';
 
 import Avatar from './Avatar';
+import Wrapper from './Wrapper';
 
 
 describe('Wrapper', () => {
    it("should render a caption and an image", () => {
       const cap = 'a beatiful cat';
-      render(<Wrapper><Avatar /></Wrapper>)
+      const alt = '@github-handle'
+      render(
+         <Wrapper caption={cap}>
+            <Avatar alt={alt} />
+         </Wrapper>
+      )
       const img = screen.getByAltText(alt)
       const caption = screen.getByText(cap)
       expect(img).toBeInTheDocument();
