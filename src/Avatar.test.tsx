09171f320df8a97f7893f605257b8eadd12d6457
Kevin Soruco <kevin-s93@hotmail.com>
2023-02-02 15:59:22 -0300

    feat
    * Add onError prop to img and manage it with a local state
    * Create test cases for onError img event

--- src/Avatar.test.tsx
+++ src/Avatar.test.tsx
@@ -1,9 +1,13 @@
-import { describe, expect, it } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { describe, expect, it, beforeEach } from 'vitest';
+import { cleanup, fireEvent, render, screen } from '@testing-library/react';
 
 import Avatar, { FALLBACK_AVATAR_ALT_TEXT, FALLBACK_AVATAR_URL } from './Avatar';
 
 describe("Avatar", () => {
+   beforeEach(() => {
+      cleanup();
+   });
+
    it("should render an <img />", () => {
       const url = 'https://cataas.com/cat/says/hello%20world!';
       const alt = '@github-handle';
@@ -27,5 +31,21 @@ describe("Avatar", () => {
       render(<Avatar />);
       const img = screen.getByAltText(FALLBACK_AVATAR_ALT_TEXT);
       expect(img).toHaveAttribute('src', FALLBACK_AVATAR_URL)
-   })
+   });
+
+   it("should use a fallback image if image fails to load", async() => {
+      render(<Avatar url="https://hello.com/fake.png" />);
+
+      const img = screen.getByAltText(FALLBACK_AVATAR_ALT_TEXT);
+      fireEvent.error(img);
+      expect(img).toHaveAttribute('src', FALLBACK_AVATAR_URL)
+   });
+
+   it("should use a fallback image if URL is an empty string", () => {
+
+      render(<Avatar url="" />);
+      const img = screen.getByAltText(FALLBACK_AVATAR_ALT_TEXT);
+      fireEvent.error(img);
+      expect(img).toHaveAttribute('src', FALLBACK_AVATAR_URL)
+   });
 })
--- src/Avatar.tsx
+++ src/Avatar.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const FALLBACK_AVATAR_ALT_TEXT = '@hello-cat';
 export const FALLBACK_AVATAR_URL = 'https://cataas.com/cat/says/hello%20world!';
@@ -9,8 +9,10 @@ type AvatarProps = {
 }
 
 function Avatar({ url = FALLBACK_AVATAR_URL, alt = FALLBACK_AVATAR_ALT_TEXT }: AvatarProps) {
+  const [src, setSrc] = useState(url);
+
   return (
-    <img src={url} alt={alt} />
+    <img src={src} alt={alt} onError={() => setSrc(FALLBACK_AVATAR_URL)} />
   )
 }
 
